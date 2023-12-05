563c6b16535a77b12c34dc36411105333e18604a
Jon Harvey <jon.harvey.uk@gmail.com>
2023-12-05 16:00:00 +0000

    Day04 pt2

--- day04.py
+++ day04.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 """
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -7,11 +9,14 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 """
 
+CARDS = []
+
 
 def get_card_points(line):
     (wint, numt) = line.split(":")[1].split(" | ")
     wins = wint.split()
     nums = numt.split()
+    CARDS.append((wins, nums))
     result = 0
     for win in wins:
         if win in nums:
@@ -23,8 +28,27 @@ def get_card_points(line):
 
 
 pt1_result = 0
+pt2_card_copies = Counter()
 with open("day04/input.txt", "r") as file:
     for line in file.read().splitlines():
         pt1_result = pt1_result + get_card_points(line)
 
+for i, card in enumerate(CARDS):
+    card_id = i + 1
+    # Add the original card as a copy.
+    pt2_card_copies[card_id] += 1
+
+    wins = card[0]
+    nums = card[1]
+    matches = 0
+    for n in nums:
+        if n in wins:
+            matches += 1
+
+    max_card_id = len(CARDS)
+    for copy_id in range(card_id + 1, card_id + matches + 1):
+        if copy_id <= max_card_id:
+            pt2_card_copies[copy_id] += pt2_card_copies[card_id]
+
 print("Part 1:", pt1_result)
+print("Part 2:", sum(pt2_card_copies.values()))
