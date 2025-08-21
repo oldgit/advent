caad0b165ac3e3d1f555839d445baa5bf6cc3716
oldgit <jon.harvey.uk@gmail.com>
2025-08-21 15:11:12 +0100

    Add day08

--- 2024/python/README.md
+++ 2024/python/README.md
@@ -3,7 +3,7 @@
 2024 solutions written in python.
 
 Input files are not included.
-Use input files provided by your [Advent of Code](https://adventofcode.com) login. 
+Use input files provided by your [Advent of Code](https://adventofcode.com) login.
 
 To run:
 
@@ -14,10 +14,12 @@ To run:
 - Add input files to the data directory, e.g. `data/day01/input.txt`
 - Add expected answers to the data directory, e.g. `data/day01/expected.txt`
 - Format for _expected.txt_ is:
+
   ```aiignore
   Part1: 10
   Part2: 20
   ```
+
 - pip install: `pip install black flake8`
 - For formatting run: `black .`
 - For linting run: `flake8 .`
--- 2024/python/day08.py
+++ 2024/python/day08.py
@@ -1,54 +1,64 @@
 import sys
+from collections import defaultdict
+from itertools import combinations
 
 from util import expected_for_day
 
 DAY = sys.argv[0].split("/")[-1].split(".")[0]
 
+
 with open(f"data/{DAY}/input.txt", "r") as file:
-    GRID = file.read().strip().split("\n")
-    MAX_ROWS = len(GRID)
-    MAX_COLS = len(GRID[0])
-    ANTENNA_MAP = {}
-    for Y, r in enumerate(GRID):
-        for X, c in enumerate(r):
-            if c != ".":
-                ls = ANTENNA_MAP.get(c, [])
-                ls.append((X, Y))
-                ANTENNA_MAP[c] = ls
+    ANTENNAS = defaultdict(list)
+    grid = [(c, x, y) for y, r in enumerate(file) for x, c in enumerate(r.strip())]
+    last_cxy = grid[-1]
+    assert last_cxy[1] == last_cxy[2], "last x & y should match for a square grid"
+    MAX = last_cxy[1]
+    {ANTENNAS[c].append((x, y)) for c, x, y in grid if c != "."}
 
 
-def add_if_in_grid(anti_node_set, x, y):
-    if 0 <= x < MAX_COLS and 0 <= y < MAX_ROWS:
-        anti_node_set.add((x, y))
+def in_grid(point):
+    return (
+        0 <= point[0] <= MAX
+        and 0 <= point[0] <= MAX
+        and 0 <= point[1] <= MAX
+        and 0 <= point[1] <= MAX
+    )
 
 
-def solve(part_2=False):
-    anti_nodes = set()
-    for freq in ANTENNA_MAP.keys():
-        antennas = ANTENNA_MAP.get(freq)
-        for i in range(len(antennas)):
-            for j in range(i + 1, len(antennas)):
-                dx, dy = (
-                    antennas[j][0] - antennas[i][0],
-                    antennas[j][1] - antennas[i][1],
-                )
-                max_inc_distance = max(abs(dx), abs(dy))
-                n = int(MAX_COLS / max_inc_distance) + 1 if part_2 else 2
-                for k in range(1, n):
-                    add_if_in_grid(
-                        anti_nodes, antennas[i][0] - (k * dx), antennas[i][1] - (k * dy)
-                    )
-                    add_if_in_grid(
-                        anti_nodes, antennas[j][0] + (k * dx), antennas[j][1] + (k * dy)
-                    )
-                if part_2:
-                    anti_nodes.add(antennas[i])
-                    anti_nodes.add(antennas[j])
-    return len(anti_nodes)
+def solve(antennas, part_2=False):
+    antinodes = set()
+
+    for coords in antennas.values():
+        for (start_y, start_x), (end_y, end_x) in combinations(coords, 2):
+            dy = start_y - end_y
+            dx = start_x - end_x
+
+            if part_2:
+                aa = (start_x, start_y)
+                i = 1
+                while in_grid(aa):
+                    antinodes.add(aa)
+                    aa = (start_x + dx * i, start_y + dy * i)
+                    i += 1
+                bb = (end_x, end_y)
+                j = 1
+                while in_grid(bb):
+                    antinodes.add(bb)
+                    bb = (end_x - dx * j, end_y - dy * j)
+                    j += 1
+            else:
+                aa = (start_x + dx, start_y + dy)
+                bb = (end_x - dx, end_y - dy)
+                if in_grid(aa):
+                    antinodes.add(aa)
+                if in_grid(bb):
+                    antinodes.add(bb)
+
+    return len(antinodes)
 
 
-p1_result = solve()
-p2_result = solve(True)
+p1_result = solve(ANTENNAS)
+p2_result = solve(ANTENNAS, True)
 
 p1_expected, p2_expected = expected_for_day(DAY)
 assert p1_result == p1_expected
